64e02cfa0fa4f4bba240834806e269c1ff8578a2
Ryan Baumann <ryan.baumann@gmail.com>
2016-06-30 08:54:21 -0400

    Hook up initial pass at location matching

--- pleiades-osm.rb
+++ pleiades-osm.rb
@@ -1,12 +1,37 @@
 #!/usr/bin/env ruby
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 ENV['OSMLIB_XML_PARSER']='Expat'
 require 'OSM/StreamParser'
 require 'OSM/Database'
+require 'OSM/objects'
+require 'time'
 require 'csv'
 
+DISTANCE_THRESHOLD = 8.0
+
+def haversine_distance(lat1, lon1, lat2, lon2)
+	km_conv = 6371 # km
+	dLat = (lat2-lat1) * Math::PI / 180
+	dLon = (lon2-lon1) * Math::PI / 180
+	lat1 = lat1 * Math::PI / 180
+	lat2 = lat2 * Math::PI / 180
+
+	a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
+	c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+	d = km_conv * c
+end
+
+def centroid(nodes)
+  avg_lat = nodes.map{|n| n.lat.to_f}.inject{ |sum, el| sum + el }.to_f / nodes.size
+  avg_lon = nodes.map{|n| n.lon.to_f}.inject{ |sum, el| sum + el }.to_f / nodes.size
+  return OSM::Node.new(1, 'pleiades-osm', Time.new.xmlschema, avg_lon, avg_lat)
+end
+
 class PleiadesCallbacks < OSM::Callbacks
-  attr_accessor :check_nodes, :database, :reparse, :check_names
+  attr_accessor :check_nodes, :database, :reparse, :pleiades_names, :pleiades_places
 
   def node(node)
     if reparse
@@ -14,9 +39,15 @@ class PleiadesCallbacks < OSM::Callbacks
         $stderr.puts node.inspect
         return true
       else
-        if node.tags['name'] && @check_names.include?(node.tags['name'])
+        if node.tags['name'] && @pleiades_names.keys.include?(node.tags['name'])
           $stderr.puts node.inspect
-          return true
+          @pleiades_names[node.tags['name']].each do |place|
+            if(haversine_distance(node.lat.to_f, node.lon.to_f, @pleiades_places[place]["reprLat"].to_f, @pleiades_places[place]["reprLong"].to_f) < DISTANCE_THRESHOLD)
+              puts "#{place},#{node.inspect}"
+              return true
+            end
+          end
+          return false
         end
       end
     end
@@ -24,9 +55,20 @@ class PleiadesCallbacks < OSM::Callbacks
   end
 
   def way(way)
-    if way.tags['name'] && @check_names.include?(way.tags['name'])
+    if way.tags['name'] && @pleiades_names.keys.include?(way.tags['name'])
       $stderr.puts way.inspect
-      unless reparse
+      if reparse
+        nodes = way.nodes.map{|n| @database.get_node(n.to_i)}.reject{|n| n.nil?}
+        way_centroid = centroid(nodes)
+        $stderr.puts way_centroid.inspect
+        @pleiades_names[way.tags['name']].each do |place|
+          if(haversine_distance(way_centroid.lat.to_f, way_centroid.lon.to_f, @pleiades_places[place]["reprLat"].to_f, @pleiades_places[place]["reprLong"].to_f) < DISTANCE_THRESHOLD)
+            puts "#{place},#{way.inspect}"
+            return true
+          end
+        end
+        return false
+      else
         @check_nodes = (@check_nodes + way.nodes.map{|n| n.to_i}).uniq
       end
       # way.nodes.each do |node|
@@ -39,7 +81,7 @@ class PleiadesCallbacks < OSM::Callbacks
   end
 
   def relation(relation)
-    if relation.tags['name'] && @check_names.include?(relation.tags['name'])
+    if relation.tags['name'] && @pleiades_names.keys.include?(relation.tags['name'])
       $stderr.puts relation.inspect
       return true
     end
@@ -47,11 +89,12 @@ class PleiadesCallbacks < OSM::Callbacks
     return false
   end
 
-  def initialize(db, names)
+  def initialize(db, names, places)
     @check_nodes = []
     @database = db
     @reparse = false
-    @check_names = names
+    @pleiades_names = names
+    @pleiades_places = places
   end
 end
 
@@ -83,7 +126,7 @@ end
 $stderr.puts place_names.keys.length
 
 db = OSM::Database.new
-cb = PleiadesCallbacks.new(db, place_names.keys)
+cb = PleiadesCallbacks.new(db, place_names, places)
 parser = OSM::StreamParser.new(:filename => osm_file, :callbacks => cb)
 $stderr.puts "Parsing OSM..."
 parser.parse
